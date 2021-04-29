d7e3554d56d0c51847780060bc76867150fe02d8
Qiang Liu <dploop@163.com>
2021-04-29 12:40:24 +0800

    fix: error to be wrapped should be e

--- client/client.go
+++ client/client.go
@@ -233,7 +233,7 @@ func (r *Result) Into(body interface{}) (courier.Metadata, error) {
 		}
 
 		if e := transformer.DecodeFromReader(r.Response.Body, rv, textproto.MIMEHeader(r.Response.Header)); e != nil {
-			return statuserror.Wrap(err, http.StatusInternalServerError, "DecodeFailed")
+			return statuserror.Wrap(e, http.StatusInternalServerError, "DecodeFailed")
 		}
 
 		return nil
